b627c1ffc9043bb57d1fe0f71abf1ac0dcfa3a55
roma-developer <ryzikov16@gmail.com>
2023-10-19 21:31:47 +0700

    finish

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const btnClose = $(".header__close-btn")
+const btnOpen = $(".header__menu-mob-btn")
+const mobMenu = $(".header__box-mob-menu")
+const body = $("body")
+
+btnOpen.on( "click", function() {
+    mobMenu.addClass("header__box-mob-menu--open")
+    body.addClass("overflow-y-off")
+})
+
+btnClose.on( "click", function() {
+    mobMenu.removeClass("header__box-mob-menu--open")
+    body.removeClass("overflow-y-off")
+})
