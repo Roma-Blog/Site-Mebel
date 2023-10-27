72786fead9978a2c1b72b7d69de92e8196939a15
Roman <ryzikov14@gmail.com>
2023-10-27 16:13:57 +0700

    mask tel new

--- js/script.js
+++ js/script.js
@@ -1,17 +1,84 @@
-const btnClose = $(".header__close-btn")
-const btnOpen = $(".header__menu-mob-btn")
-const mobMenu = $(".header__box-mob-menu")
-const body = $("body")
+const btnClose = document.querySelector(".header__close-btn")
+const btnOpen = document.querySelector(".header__menu-mob-btn")
+const mobMenu = document.querySelector(".header__box-mob-menu")
+const body = document.querySelector("body")
 
-btnOpen.on( "click", function() {
-    mobMenu.addClass("header__box-mob-menu--open")
-    body.addClass("overflow-y-off")
+btnOpen.addEventListener("click", function () {
+    mobMenu.classList.add("header__box-mob-menu--open")
+    body.classList.add("overflow-y-off")
 })
 
-btnClose.on( "click", function() {
-    mobMenu.removeClass("header__box-mob-menu--open")
-    body.removeClass("overflow-y-off")
+btnClose.addEventListener("click", function () {
+    mobMenu.classList.remove("header__box-mob-menu--open")
+    body.classList.remove("overflow-y-off")
 })
 
-//mask-phone________________________________
-$('.art-stranger').mask('+7(999)999-99-99',{autoclear: false})
+//mask-phone_______________________________
+let phoneInputs = document.querySelectorAll('input[data-tel-input]');
+
+let getInputNumbersValue = function (input) {
+    return input.value.replace(/\D/g, '');
+}
+
+let onPhonePaste = function (e) {
+    let input = e.target,
+        inputNumbersValue = getInputNumbersValue(input);
+    let pasted = e.clipboardData || window.clipboardData;
+    if (pasted) {
+        let pastedText = pasted.getData('Text');
+        if (/\D/g.test(pastedText)) {
+            input.value = inputNumbersValue;
+            return;
+        }
+    }
+}
+
+let onPhoneInput = function (e) {
+    let input = e.target,
+        inputNumbersValue = getInputNumbersValue(input),
+        selectionStart = input.selectionStart,
+        formattedInputValue = "";
+
+    if (!inputNumbersValue) {
+        return input.value = "";
+    }
+
+    if (input.value.length != selectionStart) {
+        if (e.data && /\D/g.test(e.data)) {
+            input.value = inputNumbersValue;
+        }
+        return;
+    }
+
+    if (["7", "8", "9"].indexOf(inputNumbersValue[0]) > -1) {
+        if (inputNumbersValue[0] == "9") inputNumbersValue = "7" + inputNumbersValue;
+        let firstSymbols = (inputNumbersValue[0] == "8") ? "8" : "+7";
+        formattedInputValue = input.value = firstSymbols + " ";
+        if (inputNumbersValue.length > 1) {
+            formattedInputValue += '(' + inputNumbersValue.substring(1, 4);
+        }
+        if (inputNumbersValue.length >= 5) {
+            formattedInputValue += ') ' + inputNumbersValue.substring(4, 7);
+        }
+        if (inputNumbersValue.length >= 8) {
+            formattedInputValue += '-' + inputNumbersValue.substring(7, 9);
+        }
+        if (inputNumbersValue.length >= 10) {
+            formattedInputValue += '-' + inputNumbersValue.substring(9, 11);
+        }
+    } else {
+        formattedInputValue = '+' + inputNumbersValue.substring(0, 16);
+    }
+    input.value = formattedInputValue;
+}
+let onPhoneKeyDown = function (e) {
+    let inputValue = e.target.value.replace(/\D/g, '');
+    if (e.keyCode == 8 && inputValue.length == 1) {
+        e.target.value = "";
+    }
+}
+for (let phoneInput of phoneInputs) {
+    phoneInput.addEventListener('keydown', onPhoneKeyDown);
+    phoneInput.addEventListener('input', onPhoneInput, false);
+    phoneInput.addEventListener('paste', onPhonePaste, false);
+}
